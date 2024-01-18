a0abf15317bb7df96c0939cbd29e080ad942208f
Leandro Souza <leandro.souza.159@gmail.com>
2024-01-18 14:02:15 -0300

    Add function documentation and improve code readability

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -2,13 +2,30 @@ import json
 import csv
 
 def leitura_json(path_json):
+    """
+    Função para ler um arquivo JSON e retornar os dados como uma lista de dicionários.
+
+    Args:
+        path_json (str): O caminho do arquivo JSON a ser lido.
+
+    Returns:
+        list: Os dados do arquivo JSON como uma lista de dicionários.
+    """
     dados_json = []
     with open(path_json, 'r') as file:
         dados_json = json.load(file)
     return dados_json
 
 def leitura_csv(path_csv):
+    """
+    Função que lê um arquivo CSV e retorna os dados como uma lista de dicionários.
 
+    Args:
+        path_csv (str): O caminho do arquivo CSV a ser lido.
+
+    Returns:
+        list: Uma lista de dicionários contendo os dados do arquivo CSV.
+    """
     dados_csv = []
     with open(path_csv, 'r', encoding='utf-8-sig') as file:
         spamreader = csv.DictReader(file, delimiter=',')
@@ -18,6 +35,16 @@ def leitura_csv(path_csv):
     return dados_csv
 
 def leitura_dados(path, tipo_arquivo):
+    """
+    Função para ler dados de um arquivo.
+
+    Args:
+        path (str): O caminho do arquivo.
+        tipo_arquivo (str): O tipo de arquivo ('csv' ou 'json').
+
+    Returns:
+        list: Os dados lidos do arquivo.
+    """
     dados = []
 
     if tipo_arquivo == 'csv':
@@ -29,9 +56,28 @@ def leitura_dados(path, tipo_arquivo):
     return dados
 
 def get_columns(dados):
+    """
+    Obtém a lista de nomes das colunas a partir dos dados fornecidos.
+
+    Args:
+        dados (list): Uma lista de dicionários representando os dados.
+
+    Returns:
+        list: Uma lista de nomes das colunas.
+    """
     return list(dados[-1].keys())
 
 def rename_columns(dados, key_mapping):
+    """
+    Renomeia as colunas de um conjunto de dados com base em um mapeamento de chaves fornecido.
+
+    Args:
+        dados (list[dict]): O conjunto de dados a ser processado, onde cada elemento é um dicionário representando uma linha.
+        key_mapping (dict): Um dicionário que mapeia os nomes antigos das colunas para os novos nomes das colunas.
+
+    Returns:
+        list[dict]: O conjunto de dados com as colunas renomeadas.
+    """
     new_dados_csv = []
 
     for old_dict in dados:
@@ -43,16 +89,44 @@ def rename_columns(dados, key_mapping):
     return new_dados_csv
 
 def size_data(dados):
+    """
+    Retorna o tamanho dos dados fornecidos.
+
+    Parâmetros:
+    dados (list): Os dados para calcular o tamanho.
+
+    Retorna:
+    int: O tamanho dos dados.
+    """
     return len(dados)
 
 def join(dadosA, dadosB):
-    combined_list = []
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list
+    """
+    Junta duas listas e retorna a lista combinada.
+
+    Args:
+        dadosA (list): A primeira lista a ser juntada.
+        dadosB (list): A segunda lista a ser juntada.
+
+    Returns:
+        list: A lista combinada de dadosA e dadosB.
+    """
+    lista_combinada = []
+    lista_combinada.extend(dadosA)
+    lista_combinada.extend(dadosB)
+    return lista_combinada
 
 def transformando_dados_tabela(dados, nomes_colunas):
-    
+    """
+    Transforma os dados fornecidos em um formato de tabela.
+
+    Args:
+        dados (list[dict]): Os dados a serem transformados.
+        nomes_colunas (list[str]): Os nomes das colunas na tabela.
+
+    Returns:
+        list[list]: Os dados transformados em formato de tabela.
+    """
     dados_combinados_tabela = [nomes_colunas]
 
     for row in dados:
@@ -64,6 +138,16 @@ def transformando_dados_tabela(dados, nomes_colunas):
     return dados_combinados_tabela
 
 def salvando_dados(dados, path):
+    """
+    Salva os dados fornecidos em um arquivo.
+
+    Args:
+        data (list): Os dados a serem salvos.
+        path (str): O caminho do arquivo para salvar os dados.
+
+    Returns:
+        None
+    """
     with open(path, 'w', encoding='utf-8-sig') as file:
         writer = csv.writer(file)
         writer.writerows(dados)
